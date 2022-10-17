a809ea1f65c6de0180497b1dacbeae786b44ccfc
Fisto12 <100323332+Fisto12@users.noreply.github.com>
2022-10-17 20:57:23 +0100

    Update MainUserInfo.js

--- src/components/MainUserInfo.js
+++ src/components/MainUserInfo.js
@@ -7,7 +7,7 @@ const MainUserInfo = ({ usersData }) => {
       sx={{
         height: "auto",
         margin: "40px",
-        width: {xs:"340px",lg:'auto'},
+        width: {xs:"330px",lg:'auto'},
       }}
     >
       <Box
